e08a75e940868245fb50e0a8ccd62a6c7e706330
Shadow-1337 <rtre4tdt4@gmail.com>
2014-12-31 03:14:39 +0100

    Testing of http save

--- Tester.py
+++ Tester.py
@@ -9,6 +9,9 @@ import re
 import os
 import atexit
 import MySQLdb
+import urllib2
+import urllib
+import json
 
 
 port = 9433
@@ -66,6 +69,25 @@ clientsocket.close()
 
 
 
+print urllib2.urlopen("http://localhost:890/DayZServlet/lud0/load/?uid=33683593567475533&id=000000").read()
+
+
+
+
+
+url = 'http://localhost:890/DayZServlet/lud0/create/?uid=33683593567475533&id=000000'
+data = '{"model":"SurvivorPartsMaleWhite","alive":1,"pos":[13157.1,6.00144,7128.86],"dir":[0.890747,0,0.454499],"up":[0,1,0],"items":[{"slot":"body","type":"TShirtBlue"},{"slot":"legs","type":"HunterPants_Winter","items":[{"slot":"supply","type":"Tool_Flashlight","items":[{"slot":"batteryd","type":"Consumable_Battery9V","state":{"vars":{"power":30000}}}]}],"state":{"damage":0.223643}},{"slot":"feet","type":"AthleticShoes_Grey"}],"state":{"vars":{"exposure":0,"modifiers":["Hunger"],"modstates":[[[0],-1,87]],"bloodtype":"BloodONeg","blood":5000,"health":5000,"shock":0,"energy":932.957,"water":1689.78,"stomach":836,"diet":0.5,"unconscious":false,"mynotifiers":[false,false,["hungry",[0.525,0.541,0.031,1]]],"damagearray":[]}}}'
+req = urllib2.Request(url)
+req.add_header('Content-Type', 'application/json')
+response = urllib2.urlopen(req, data)
+print response.read()
+
+url = 'http://localhost:890/DayZServlet/lud0/save/?uid=33683593567475533&id=000000'
+data = '{"model":"SurvivorPartsMaleWhite","alive":1,"pos":[13157.1,6.00144,7128.86],"dir":[0.890747,0,0.454499],"up":[0,1,0],"items":[{"slot":"body","type":"TShirtBlue"},{"slot":"legs","type":"HunterPants_Winter","items":[{"slot":"supply","type":"Tool_Flashlight","items":[{"slot":"batteryd","type":"Consumable_Battery9V","state":{"vars":{"power":30000}}}]}],"state":{"damage":0.223643}},{"slot":"feet","type":"AthleticShoes_Grey"}],"state":{"vars":{"exposure":0,"modifiers":["Hunger"],"modstates":[[[0],-1,87]],"bloodtype":"BloodONeg","blood":5000,"health":5000,"shock":0,"energy":932.957,"water":1689.78,"stomach":836,"diet":0.5,"unconscious":false,"mynotifiers":[false,false,["hungry",[0.525,0.541,0.031,1]]],"damagearray":[]}}}'
+req = urllib2.Request(url)
+req.add_header('Content-Type', 'application/json')
+response = urllib2.urlopen(req, data)
+print response.read()
 
 
 time.sleep(1000)
